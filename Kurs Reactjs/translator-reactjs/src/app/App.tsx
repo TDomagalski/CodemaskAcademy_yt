050b12ca20a23f882778eff471b7f9783f02334d
Tomasz Domagalski <tomekwebdev@gmail.com>
2022-06-14 15:41:33 +0200

    feat: add styled-components, create color-palette, first types in ts  /  https://www.youtube.com/watch?v=3XcByTIhv_0&t=26s

--- Kurs Reactjs/translator-reactjs/src/app/App.tsx
+++ Kurs Reactjs/translator-reactjs/src/app/App.tsx
@@ -1,5 +1,17 @@
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../lib/styles";
+
 export const App = () => (
-	<div>
-		<p>Hello World!</p>
-	</div>
+	<ThemeProvider theme={theme}>
+		<AppContainer>
+			<span>Hello World!!!</span>
+		</AppContainer>
+	</ThemeProvider>
 );
+
+// STYLED-COMPONENTS
+const AppContainer = styled.div`
+	width: 100%;
+	height: 100vh;
+	background-color: ${({ theme }) => theme.colors.background};
+`;
