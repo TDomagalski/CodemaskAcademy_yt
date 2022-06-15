04392796db60421e975b175a1bf35758d7ed2813
Tomasz Domagalski <tomekwebdev@gmail.com>
2022-06-15 16:06:36 +0200

    fix: comments unused lines

--- Kurs Reactjs/translator-reactjs/src/features/translator/TranslatorScreen.tsx
+++ Kurs Reactjs/translator-reactjs/src/features/translator/TranslatorScreen.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { useTranslations } from "lib/hooks";
+// import { useTranslations } from "lib/hooks";
 
 export const TranslatorScreen: React.FunctionComponent = () => {
 	// Używanie hooka o nazwie useTranslation którego zaimportowaliśmy z kat. lib/hooks
-	const T = useTranslations();
+	// const T = useTranslations();
 
 	return (
 		// Komponent musi coś zwracać, można ustawić return null. W spanie za pomocą {} dostaje się do hooka useTranslations(), który jest w zmiennej o nazwie T
 		<Container>
-			<span>{T.appName}</span>
+			<span>Hello World</span>
 		</Container>
 	);
 };
 
 const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex: 1;
 	color: ${({ theme }) => theme.colors.typography};
 `;
